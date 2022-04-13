6e22fb2f721446e7051e80bb97c83566b8d906e6
DongwooKim <kimphysicsman@gmail.com>
2022-04-13 13:24:40 +0900

    naver map crawling

--- main.py
+++ main.py
@@ -1,88 +1,23 @@
-# 여행 경로 그래프
-dists = [[0, 7, 3, 2],
-         [7, 0, 5, 4],
-         [3, 5, 0, 6],
-         [2, 4, 6, 0]]
+import parsing
+import tsp
 
-# 여행 장소 개수
-N = len(dists)
+place = ['서울역', '경복궁', '남산타워', '광화문']
 
-# 장소를 방문한 순서
-list_visited = []
+N = len(place)
 
-# 모두 여행한 상태
-visited_all = (1 << N) - 1
+dists = [[0 for col in range(N)] for row in range(N)]
 
-# cache[i][j] : i번째 장소에서 j의 상태일 때 출발 장소로 가는 최적경로 비용
-cache = [[None] * (1 << N) for _ in range(N)]
-INF = float('inf')
-
-
-def path(last, visited):
-    # last : 마지막으로 여행한 장소 = 현재 장소
-    # visited : 지금까지 여행한 장소들
-    # path(last, visited) : visited 상태에서 현재 last에 있을 때
-    #                       출발 장소로 돌아오는 경로 반환
-
-    # 지금까지 여행한 장소들이 모두 여행한 상태와 같다면
-    # 출발한 장소로 돌아오거나 경로가 없으면 무한대로 반환
-    if visited == visited_all:
-        return dists[last][0] or INF
-
-    # cache[last][visited] : visited 상태에서 현재 장소가 last에 있을 때의 최적경로
-    # 이미 최적 경로가 있다면 그 경로를 반환
-    if cache[last][visited] is not None:
-        return cache[last][visited]
-
-    temp = INF
-    for dist in range(N):
-        # dist : 다음 장소
-        # visited & (1 << dist) == 0 : 다음 장소를 방문한 적 없고
-        # dists[last][dist] != 0 : 현재 장소에서 다른 장소로의 경로가 존재할 때
-        if visited & (1 << dist) == 0 and dists[last][dist] != 0:
-            # visited | (1 << dist) : 다음 장소를 방문한 상태
-            # path(dist, visited | (1 << dist)) : 다음 장소에서 출발 장소로 가는 최적 경로
-            # path(dist, visited | (1 << dist)) + dists[last][dist]
-            #   : 현재 장소에서 다음 장소로 이동한 뒤, 다음 장소에서 출발 장소로 가는 최적경로
-            # temp : 현재 장소에서 아직 가지않은 장소들을 순회하며 계산한 경로의 최소값
-            # temp2 = temp
-            # print('called path', last, '->', dist)
-            temp = min(temp, path(dist, visited | (1 << dist)) + dists[last][dist])
-            # temp3 = visited
-            # count = 0
-            # if temp2 != temp:
-            #     while temp3 != 0:
-            #         temp3 = temp3 & (temp3 - 1)
-            #         count += 1
-            #     print(last, dist, count, bin(visited), '  update :', temp2, '->', temp)
-    # temp : 현재 장소에서 출발 장소로 가는 최적경로
-    cache[last][visited] = temp
-    return temp
-
-
-distance = path(0, 1 << 0)
-
-current_place = 0
-visited_place = 1
-path_order = [0]
 for i in range(N):
     for j in range(N):
-        if 1 & (1 << j):  # 현재 방문한 곳이 이미 방문한 곳이면 넘어가기
-            continue
-        if distance - dists[current_place][j] == cache[j][visited_place + (1 << j)]:
-            path_order.append(j)
-            # print('path update', distance, current_place, j, cache[j][visited_place + (1 << j)])
-            # print('path update', bin(visited_place), bin(visited_place + (1 << j)))
-            distance = cache[j][visited_place + (1 << j)]
-            current_place = j
-            visited_place += (1 << j)
+        if i != j:
+            dists[i][j] = parsing.route(place[i], place[j])
 
-k = 0
-last_place = 15 ^ visited_place
-print(last_place)
-while last_place != 1:
-    last_place = last_place >> 1
-    k += 1
-path_order.append(k)
+result = tsp.tsp(dists)
 
-print(path_order)
+route = []
+for i in result[1]:
+    route.append(place[i])
+
+print('총 이동 시간 :', result[0])
+print('여행 경로 :', route)
+
--- parsing.py
+++ parsing.py
@@ -1,9 +1,42 @@
 import json
-import requests as r
+import requests
 
-http_header = {
-    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-    'x-requested-with': 'XMLHttpRequest'
-}
-session = r.Session()
-session.headers.update(http_header)
+base_parsing_url = 'https://map.naver.com/v5/api/search?caller=pcweb&query='
+base_route_url = 'https://map.naver.com/v5/api/transit/directions/point-to-point?start={}, {},placeid= {},name= {}&goal= {}, {},placeid= {},name={}'
+
+# 네이버지도에서 word를 검색하여 첫번째 장소의 정보 크롤링
+def parsing(word):
+    parsing_url = base_parsing_url + word
+    data = requests.get(parsing_url).json()
+
+    place = data['result']['place']['list'][0]
+    id = place['id']
+    name = place['name']
+    x = place['x']
+    y = place['y']
+    address = place['address']
+
+    return [x, y, id, name]
+
+
+# start에서 goal까지 걸리는 이동 시간
+def route(start_word, goal_word):
+    start = parsing(start_word)
+    goal = parsing(goal_word)
+
+    route_url = base_route_url.format(start[0], start[1], start[2], start[3],
+                                      goal[0], goal[1], goal[2], goal[3])
+
+    data = requests.get(route_url).json()
+    print('start :', start[3])
+    print('goal :', goal[3])
+    if data['paths']:
+        duration = data['paths'][0]['duration']
+    else:
+        duration = 1
+    print('duration :', duration)
+
+    return duration
+
+
+# route(parsing('서울역'), parsing('경복궁'))
