9e05153e368941d1aeb2992291d25a804b0c6f08
DongwooKim <kimphysicsman@gmail.com>
2022-04-15 09:08:34 +0900

    time schedule commit

--- examples/time_example.py
+++ examples/time_example.py
@@ -37,10 +37,8 @@ next_index = current_index + 1
 next_point = route[next_index]
 
 # 장소 추가 유무 변수 - 0: 추가안해도됨, 1:추가해야됨
-lunch_bool = 1
-dinner_bool = 1
-caffe_bool = 0
-hotel_bool = 0
+add_place_index = [1, 1, 1, 1]
+lunch_bool, dinner_bool, caffe_bool, hotel_bool = add_place_index
 
 # 장소 추가 시점 변수
 lunch_time = current_day + t(hours=12)
@@ -53,11 +51,10 @@ real_route = [0]
 # real_places = [places[0]]
 # real_info = [places_info[0]]
 
-# 다음 이동할 장소의 종류 - 0: 여행 장소, 1: 맛집, 2:카페, 3:숙소
+# 다음으로 이동할 장소의 종류 - 0: 여행 장소, 1: 맛집, 2:카페, 3:숙소
 index = 0
 
 while 1:
-
     # 다음 이동 장소 판단
     if lunch_bool and current_time > lunch_time:
         index = 1
@@ -129,15 +126,27 @@ while 1:
             current_time += t(minutes=duration)
             print(current_time, ':', places[current_point], '->', add_place['name'])
 
-            # 이동한 위치 저장
-            add_point = len(places)
-            current_info = add_place
-            places.append(add_place['name'])
-            real_route.append(add_point)
-            # real_places.append(places[add_point])
-            # real_info.append(add_place)
+        # 추가한 장소 -> 추가한 장소
+        else:
 
-            # 소요 시간 업데이트
+            # 이동할 추가한 장소
+            add_place = parsing.parsing(index, places[current_point], places_info)
+
+            # 이동 시간
+            duration = parsing.duration_minute(current_info['index'], places[current_point], index, add_place['word'], places_info)
+
+            # 시간 업데이트
+            current_time += t(minutes=duration)
+            print(current_time, ':', current_info['name'], '->', add_place['name'])
+
+        # 이동한 위치 저장
+        add_point = len(places)
+        current_info = add_place
+        places.append(add_place['name'])
+        real_route.append(add_point)
+
+        # 소요 시간 업데이트
+        if index == 1:
             current_time += t(hours=1)
             print(current_time, ': 식사 시간')
 
@@ -146,157 +155,23 @@ while 1:
             else:
                 if dinner_bool:
                     dinner_bool = 0
+        elif index == 2:
+            current_time += t(hours=1)
+            print(current_time, ': 카페 시간')
+            caffe_bool = 0
+        elif index == 3:
+            current_day += t(days=1)
+            current_time = current_day + t(hours=10)
+            lunch_time = current_day + t(hours=12)
+            dinner_time = current_day + t(hours=18)
+            caffe_time = current_day + t(hours=15)
+            hotel_time = current_day + t(hours=21)
+            c = add_place_index
+            print(current_time, ': 숙소 시간')
 
-        # 추가한 장소 -> 추가한 장소
-        else:
-            print('add place -> add place')
 
 
-
-
-
-# print(places)
+print(places)
 # print(places_info)
-# print(real_route)
-# print(real_places)
-# for i in real_info:
-#     print(i)
-
-
-
-
-
-
-
-#
-#
-#
-#
-# # index : 다음 이동할 장소의 종류
-# def add_more(index):
-#     global current_point, current_index, current_time
-#     global next_point, next_index, places_info
-#
-#     if place_info['index'] == 0 and index == 0:
-#         # 여행 장소 -> 여행 장소
-#
-#         # 이동 시간
-#         move_time = dists[current_point][next_point]
-#
-#         # 시간 업데이트
-#         current_time += t(minutes=move_time)
-#         print(current_time, ':', places[current_point], '->', places[next_point])
-#
-#     elif place_info['index'] == 0 and index != 0:
-#         # 여행 장소 -> (다른 장소)
-#
-#         # 추가할 장소
-#         add_place = parsing.parsing(index, places[current_point])
-#
-#         # 이동 시간
-#         duration = parsing.duration_minute(0, places[current_point], index, add_place['word'])
-#
-#         # 시간 업데이트
-#         current_time += t(minutes=duration)
-#         print(current_time, ':', places[current_point], '->', add_place['name'])
-#
-#         # 위치 저장
-#         add_point = len(places)
-#         places.append(add_place['name'])
-#         real_route.append(add_point)
-#         real_places.append(places[add_point])
-#
-#     # 현재 장소 -> 맛집 이동시간 업데이트
-#     add_place = parsing.parsing(index, places[current_point])
-#     duration = parsing.duration_minute(0, places[current_point], index, add_place['word'])
-#     current_time += t(minutes=duration)
-#     print(current_time, ':', places[current_point], '->', add_place['name'])
-#
-#     # 맛집 위치 저장
-#     add_point = len(places)
-#     places.append(add_place['name'])
-#     real_route.append(add_point)
-#     real_places.append(places[add_point])
-#
-#     # 시간 업데이트
-#     if index == 3:
-#         current_time = current_day + t(days=1, hours=10)
-#         print(current_time, ':', '숙소 시간')
-#     elif index == 1:
-#         current_time += t(hours=1)
-#         print(current_time, ':', '식사 시간')
-#     else:   # index == 2:
-#         current_time += t(hours=1)
-#         print(current_time, ':', '카페 시간')
-#
-#     # 맛집 -> 다음 장소 이동시간 업데이트
-#     duration = parsing.duration_minute(index, add_place['word'], 0, places[next_point])
-#     current_time += t(minutes=duration)
-#     print(current_time, ':', add_place['name'], '->', places[next_point])
-#
-#     return 0
-#
-#
-# while 1:
-#     # 1 loop =  다음 장소로 이동 후 이동 시간 + 소요 시간까지 업데이트
-#
-#     if hotel_bool and current_time > hotel_time:
-#         # current -> 숙소 -> next
-#         hotel_bool = add_more(3)
-#
-#         # 날짜 변경에 따른 변수 변경
-#         current_day += t(days=1)
-#         lunch_time = current_day + t(hours=12)
-#         dinner_time = current_day + t(hours=18)
-#         caffe_time = current_day + t(hours=15)
-#         hotel_time = current_day + t(hours=21)
-#         lunch_bool = 1
-#         dinner_bool = 1
-#         caffe_bool = 1
-#         hotel_bool = 1
-#
-#     # 현재 시각에 따른 경로 추가 - 점심
-#     elif lunch_bool and current_time > lunch_time:
-#         # current -> 맛집 -> next
-#         lunch_bool = add_more(1)
-#
-#     # 현재 시각에 따른 경로 추가 - 저녁
-#     elif dinner_bool and current_time > dinner_time:
-#         # current -> 맛집 -> next
-#         dinner_bool = add_more(1)
-#
-#     # 현재 시각에 따른 경로 추가 - 카페
-#     elif caffe_bool and current_time > caffe_time:
-#         # current -> 카페 -> next
-#         caffe_bool = add_more(2)
-#
-#     # 바로 다음 장소로 이동
-#     else:
-#         # current -> next
-#
-#
-#     # 인덱스 수정
-#     current_index = next_index
-#     current_point = route[current_index]
-#
-#     # 이동한 위치 저장
-#     real_route.append(current_point)
-#     real_places.append(places[current_point])
-#
-#     # 다음 장소 이동
-#     if current_index < len(route) - 1:
-#         next_index = current_index + 1
-#         next_point = route[next_index]
-#
-#         # 관광 시간 업데이트
-#         current_time += t(hours=3)
-#         print(current_time, ':', places[current_point], '관광')
-#
-#     if current_index == len(route) - 1:
-#         break
-#
-# print(places)
-# print(real_route)
-# print(real_places)
-#
+print(real_route)
 
